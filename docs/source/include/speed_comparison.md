0bd0e5adaf85c0ab685f97378de11fb9f0ca75d3
juliendenize <julien.denize@hotmail.fr>
2023-08-24 19:38:11 +0200

    Improve speed comparison script and update the summary table

--- docs/source/include/speed_comparison.md
+++ docs/source/include/speed_comparison.md
@@ -6,7 +6,7 @@ We used a [python script](../speed_script.py) that compare the speed between Tor
 
 It **does not test** the improvement based on eliminating the CPU/GPU synchronization which should favor Torchaug in comparison with Torchvision.
 
-It was performed on **1 GPU P5000** and **6 logical cores of an Intel(R) Xeon(R) Gold 6126 CPU @ 2.60GHz**.
+It was performed on **1 GPU A100** and **8 logical cores of an Intel(R) Xeon(R) Silver 4215R CPU @ 3.20GHz**.
 
 ## Batch Transforms
 
@@ -25,36 +25,36 @@ In our implementation, there is no need to iterate over the elements, but some a
 Time in ms averaged on 100 runs for Torchvision and Torchaug.
 ### CPU
 
-| Augmentation              | Random Calls | Torchvision | Torchaug     |
-| ------------------------- | :----------: | :---------: | :------:     |
-| BatchRandomColorJitter    | 1            | x           | 819.726      |
-| BatchRandomColorJitter    | 8            | x           | 660.376      |
-| BatchRandomColorJitter    | B            | 816.436     | **754.716**  |
-| BatchRandomGaussianBlur   | B            | 1953.318    | **1570.750** |
-| BatchRandomGrayScale      | B            | 88.471      | **20.969**   |
-| BatchRandomHorizontalFlip | B            | 73.019      | **18.075**   |
-| BatchRandomResizedCrop    | 1            | x           | 117.221      |
-| BatchRandomResizedCrop    | 8            | x           | 109.775      |
-| BatchRandomResizedCrop    | 16           | x           | 118.750      |
-| BatchRandomResizedCrop    | B            | 199.716     | **189.726**  |
-| BatchRandomSolarize       | B            | 113.437     | **38.263**   |
+| Transform                 | num_rand_calls   | Torchvision (ms)    | Torchaug (ms)           |
+|---------------------------|------------------|---------------------|-------------------------|
+| BatchRandomColorJitter    | 1                |                     | 556.25 +/- 37.48        |
+| BatchRandomColorJitter    | 8                |                     | 501.83 +/- 14.29        |
+| BatchRandomColorJitter    | -1               | 1632.88 +/- 8444.97 | **1559.92 +/- 6900.16** |
+| BatchRandomGaussianBlur   |                  | 383.29 +/- 42.33    | **125.64 +/- 5.01**     |
+| BatchRandomGrayScale      |                  | 39.91 +/- 6.48      | **10.41 +/- 3.55**      |
+| BatchRandomHorizontalFlip |                  | 16.81 +/- 5.00      | **8.40 +/- 3.39**       |
+| BatchRandomResizedCrop    | 1                | 64.16 +/- 12.34     |
+| BatchRandomResizedCrop    | 8                |                     | 45.78 +/- 3.23          |
+| BatchRandomResizedCrop    | 16               |                     | 53.74 +/- 3.40          |
+| BatchRandomResizedCrop    | -1               | 114.29 +/- 4.66     | **110.43 +/- 4.34**     |
+| BatchRandomSolarize       |                  | 29.58 +/- 4.46      | **17.98 +/- 3.59**      |
+
 
 ### GPU
 
-| Augmentation              | Random Calls | Torchvision | Torchaug   |
-| ------------------------- | :----------: | :---------: | :------:   |
-| BatchRandomColorJitter    | 1            | x           | 24.430     |
-| BatchRandomColorJitter    | 8            | x           | 25.274     |
-| BatchRandomColorJitter    | B            | 35.838      | **26.155** |
-| BatchRandomGaussianBlur   | B            | 40.826      | **26.870** |
-| BatchRandomGrayScale      | B            | 3.140       | **0.718**  |
-| BatchRandomHorizontalFlip | B            | 1.485       | **0.562**  |
-| BatchRandomResizedCrop    | 1            | x           | 1.568      |
-| BatchRandomResizedCrop    | 8            | x           | 1.780      |
-| BatchRandomResizedCrop    | 16           | x           | 2.279      |
-| BatchRandomResizedCrop    | B            | 7.018       | **6.888**  |
-| BatchRandomSolarize       | B            | 1.843       | **0.428**  |
-
+| Transform                 | num_rand_calls   | Torchvision (ms) | Torchaug (ms)      |
+|---------------------------|------------------|------------------|--------------------|
+| BatchRandomColorJitter    | 1                |                  | 4.23 +/- 0.30      |
+| BatchRandomColorJitter    | 8                |                  | 8.25 +/- 0.13      |
+| BatchRandomColorJitter    | -1               | 29.91 +/- 4.13   | **19.65 +/- 0.41** |
+| BatchRandomGaussianBlur   |                  | 8.07 +/- 1.15    | **2.92 +/- 0.04**  |
+| BatchRandomGrayScale      |                  | 2.70 +/- 0.30    | **0.19 +/- 0.00**  |
+| BatchRandomHorizontalFlip |                  | 1.10 +/- 0.09    | **0.18 +/- 0.00**  |
+| BatchRandomResizedCrop    | 1                |                  | 0.56 +/- 0.13      |
+| BatchRandomResizedCrop    | 8                |                  | 1.09 +/- 0.015     |
+| BatchRandomResizedCrop    | 16               |                  | 2.06 +/- 0.021     |
+| BatchRandomResizedCrop    | -1               | 6.44 +/- 0.30    | **6.21 +/- 0.18**  |
+| BatchRandomSolarize       |                  | 1.46 +/- 0.14    | **0.22 +/- 0.01**  |
 ## Transforms
 
 Input `torch.randn(3, 224, 224)`.
@@ -62,18 +62,19 @@ Input `torch.randn(3, 224, 224)`.
 Time in ms averaged on 1000 runs for Torchvision and Torchaug.
 
 ### CPU
-| Augmentation       | Torchvision | Torchaug   |
-| ------------------ | :---------: | :------:   |
-| Normalize          | 0.696       | **0.398**  |
-| RandomColorJitter  | 12.264      | **11.584** |
-| RandomGaussianBlur | 30.934      | **30.096** |
-| RandomSolarize     | 0.927       | **0.881**  |
+| Transform           | Torchvision (ms)   | Torchaug (ms)       |
+|---------------------|--------------------|---------------------|
+| Normalize           | 42.69 +/- 86.65    | **0.43 +/- 1.59**   |
+| RandomColorJitter   | 49.49 +/- 516.74   | **11.29 +/- 11.58** |
+| RandomGaussianBlur  | 15.46 +/- 61.11    | **5.53 +/- 6.05**   |
+| RandomSolarize      | **0.51 +/- 1.72**  | 0.53 +/- 1.76       |
+
 
 ### GPU
 
-| Augmentation       | Torchvision | Torchaug  |
-| ------------------ | :---------: | :------:  |
-| Normalize          | 0.190       | **0.104** |
-| RandomColorJitter  | 1.227       | **0.727** |
-| RandomGaussianBlur | **0.672**   | 0.699     |
-| RandomSolarize     | 0.037       | **0.034** |
+| Transform          | Torchvision (ms)  | Torchaug (ms)     |
+|--------------------|-------------------|-------------------|
+| Normalize          | 0.14 +/- 0.11     | **0.07 +/- 0.08** |
+| RandomColorJitter  | 1.26 +/- 24.85    | **0.45 +/- 0.45** |
+| RandomGaussianBlur | **0.18 +/- 0.20** | 0.25 +/- 0.23     |
+| RandomSolarize     | **0.04 +/- 0.02** | 0.04 +/- 0.03     |
--- docs/source/speed_script.py
+++ docs/source/speed_script.py
@@ -1,5 +1,9 @@
-import time
+from __future__ import annotations
 
+import time
+from typing import Callable
+
+import tabulate
 import torch
 from torchvision import transforms as T
 
@@ -8,194 +12,317 @@ from torchaug import transforms as F
 
 torch.set_printoptions(precision=3)
 
-runs = 1000
+import argparse
 
 
-torchvision_transforms = [
-    T.Normalize([225], [225]),
-    T.RandomApply([T.ColorJitter(0.5, 0.5, 0.5, 0.1)], 0.5),
-    T.RandomApply([T.GaussianBlur([23, 23], [0.1, 2.0])], 0.5),
-    T.RandomSolarize(0.5, 0.5),
-]
-
-cpu_transforms = [
-    F.Normalize([225], [225]),
-    F.RandomColorJitter(0.5, 0.5, 0.5, 0.1, 0.5),
-    F.RandomGaussianBlur([23, 23], [0.1, 2.0], 0.5),
-    F.RandomSolarize(0.5, 0.5),
-]
-
-cuda_transforms = [
-    F.Normalize([225], [225]).cuda(),
-    F.RandomColorJitter(0.5, 0.5, 0.5, 0.1, 0.5).cuda(),
-    F.RandomGaussianBlur([23, 23], [0.1, 2.0], 0.5).cuda(),
-    F.RandomSolarize(0.5, 0.5).cuda(),
-]
-
-input = torch.randn(3, 224, 224)
-cuda_input = input.cuda()
-
-
-for t in torchvision_transforms:
-    results = torch.zeros(runs)
-    for i in range(runs):
+def synchro_if_cuda(device: torch.device):
+    if device.type == "cuda":
         torch.cuda.synchronize()
+    return
+
+
+def time_transform(n_runs: int, tensor: torch.Tensor, transform: Callable):
+    results = torch.zeros(n_runs)
+    for i in range(n_runs):
+        synchro_if_cuda(tensor.device)
         start = time.process_time()
-        t(input)
-        torch.cuda.synchronize()
+
+        transform(tensor)
+
+        synchro_if_cuda(tensor.device)
         end = time.process_time()
+
         results[i] = end - start
     results *= 1000
-    print(f"CPU {type(t)} in {results.mean()} +/- {torch.std(results)} ms")
 
-for t in torchvision_transforms:
-    results = torch.zeros(runs)
-    for i in range(runs):
-        torch.cuda.synchronize()
+    return results
+
+
+def time_batch_transform_tv(n_runs: int, tensor: torch.Tensor, transform: Callable):
+    results = torch.zeros(n_runs)
+    for i in range(n_runs):
+        synchro_if_cuda(tensor.device)
         start = time.process_time()
-        t(cuda_input)
-        torch.cuda.synchronize()
+
+        torch.stack([transform(img) for img in tensor])
+
+        synchro_if_cuda(tensor.device)
         end = time.process_time()
+
         results[i] = end - start
     results *= 1000
-    print(f"CUDA {type(t)} in {results.mean()} +/- {torch.std(results)} ms")
 
-for t in cpu_transforms:
-    results = torch.zeros(runs)
-    for i in range(runs):
-        torch.cuda.synchronize()
-        start = time.process_time()
-        t(input)
-        torch.cuda.synchronize()
-        end = time.process_time()
-        results[i] = end - start
-    results *= 1000
-    print(f"CPU {type(t)} in {results.mean()} +/- {torch.std(results)} ms")
+    return results
 
 
-for t in cuda_transforms:
-    results = torch.zeros(runs)
-    for i in range(runs):
-        torch.cuda.synchronize()
-        start = time.process_time()
-        t(cuda_input)
-        torch.cuda.synchronize()
-        end = time.process_time()
-        results[i] = end - start
-    results *= 1000
-    print(f"CUDA {type(t)} in {results.mean()} +/- {torch.std(results)} ms")
+if __name__ == "__main__":
 
+    parser = argparse.ArgumentParser(
+        description="Speed comparison between Torchvision and Torchaug.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
+    parser.add_argument(
+        "--n-runs-single",
+        required=False,
+        type=int,
+        default=1000,
+        help="Number of runs to compute metrics on single transforms.",
+    )
+    parser.add_argument(
+        "--n-runs-batch",
+        required=False,
+        type=int,
+        default=100,
+        help="Number of runs to compute metrics on batch transforms.",
+    )
+    parser.add_argument(
+        "--run-single",
+        required=False,
+        help="Whether to run single transforms.",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--run-batch",
+        required=False,
+        help="Whether to run batch transforms.",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--shape",
+        required=False,
+        help="Shape of the input. Batch not included.",
+        type=int,
+        nargs="+",
+        default=[3, 224, 224],
+    )
+    parser.add_argument(
+        "--batch-size",
+        required=False,
+        help="Batch size for batch input.",
+        type=int,
+        default=64,
+    )
+    parser.add_argument(
+        "--device", required=False, help="Device for input.", type=str, default="cpu"
+    )
 
-batch_input = torch.randn(64, 3, 224, 224)
-batch_input_cuda = batch_input.cuda()
+    args = parser.parse_args()
 
-batch_torchvision_transforms = [
-    T.RandomApply([T.ColorJitter(0.5, 0.5, 0.5, 0.1)], 0.5),
-    T.RandomApply([T.GaussianBlur([23, 23], [0.1, 2.0])], 0.5),
-    T.RandomApply([T.Grayscale(num_output_channels=3)], 0.5),
-    T.RandomHorizontalFlip(0.5),
-    T.RandomResizedCrop([224, 224], antialias=True),
-    T.RandomSolarize(0.5, 0.5),
-]
+    n_runs_single: int = args.n_runs_single
+    n_runs_batch: int = args.n_runs_batch
+    run_single: bool = args.run_single
+    run_batch: bool = args.run_batch
+    shape: list[int] = args.shape
+    batch_size: int = args.batch_size
+    device = args.device
 
-cpu_batch_transforms = [
-    BF.BatchRandomColorJitter(0.5, 0.5, 0.5, 0.1, 0.5, 1, inplace=True),
-    BF.BatchRandomColorJitter(0.5, 0.5, 0.5, 0.1, 0.5, 8, inplace=True),
-    BF.BatchRandomColorJitter(0.5, 0.5, 0.5, 0.1, 0.5, -1, inplace=True),
-    BF.BatchRandomGaussianBlur([23, 23], [0.1, 2], 0.5, inplace=True),
-    BF.BatchRandomGrayScale(0.5, inplace=True),
-    BF.BatchRandomHorizontalFlip(0.5, inplace=True),
-    BF.BatchRandomResizedCrop([224, 224], num_rand_calls=1),
-    BF.BatchRandomResizedCrop([224, 224], num_rand_calls=8),
-    BF.BatchRandomResizedCrop([224, 224], num_rand_calls=16),
-    BF.BatchRandomResizedCrop([224, 224], num_rand_calls=-1),
-]
+    print(args)
 
-cuda_batch_transforms = [
-    BF.BatchRandomColorJitter(0.5, 0.5, 0.5, 0.1, 0.5, 1, inplace=True).cuda(),
-    BF.BatchRandomColorJitter(0.5, 0.5, 0.5, 0.1, 0.5, 8, inplace=True).cuda(),
-    BF.BatchRandomColorJitter(0.5, 0.5, 0.5, 0.1, 0.5, -1, inplace=True).cuda(),
-    BF.BatchRandomGaussianBlur([23, 23], [0.1, 2], 0.5, inplace=True).cuda(),
-    BF.BatchRandomGrayScale(0.5, inplace=True).cuda(),
-    BF.BatchRandomHorizontalFlip(0.5, inplace=True).cuda(),
-    BF.BatchRandomResizedCrop([224, 224], num_rand_calls=1).cuda(),
-    BF.BatchRandomResizedCrop([224, 224], num_rand_calls=8).cuda(),
-    BF.BatchRandomResizedCrop([224, 224], num_rand_calls=16).cuda(),
-    BF.BatchRandomResizedCrop([224, 224], num_rand_calls=-1).cuda(),
-]
+    if run_single:
+        mono_transforms = [
+            (
+                "Normalize",
+                T.Normalize([225], [225]),
+                F.Normalize([225], [225]).to(device=device),
+            ),
+            (
+                "RandomColorJitter",
+                T.RandomApply([T.ColorJitter(0.5, 0.5, 0.5, 0.1)], 0.5),
+                F.RandomColorJitter(0.5, 0.5, 0.5, 0.1, 0.5).to(device=device),
+            ),
+            (
+                "RandomGaussianBlur",
+                T.RandomApply([T.GaussianBlur([23, 23], [0.1, 2.0])], 0.5),
+                F.RandomGaussianBlur([23, 23], [0.1, 2.0], 0.5).to(device=device),
+            ),
+            (
+                "RandomSolarize",
+                T.RandomSolarize(0.5, 0.5),
+                F.RandomSolarize(0.5, 0.5).to(device=device),
+            ),
+        ]
 
-runs = 100
+        input = torch.randn(shape, device=device)
 
-# for t in batch_torchvision_transforms:
-# 	results = torch.zeros(runs)
-# 	for i in range(runs):
-# 		torch.cuda.synchronize()
-# 		start = time.process_time()
-# 		t(batch_input)
-# 		torch.cuda.synchronize()
-# 		end = time.process_time()
-# 		results[i] = end - start
-# 	results *= 1000
-# 	print(f"all CPU {type(t)} in {results.mean()} +/- {torch.std(results)} ms")
+        rows = []
+        print("Testing mono transforms")
+        for name, torchvision_transform, torchaug_transform in mono_transforms:
+            print(name)
 
-# for t in batch_torchvision_transforms:
-# 	results = torch.zeros(runs)
-# 	for i in range(runs):
-# 		torch.cuda.synchronize()
-# 		start = time.process_time()
-# 		t(batch_input_cuda)
-# 		torch.cuda.synchronize()
-# 		end = time.process_time()
-# 		results[i] = end - start
-# 	results *= 1000
-# 	print(f"all CUDA {type(t)} in {results.mean()} +/- {torch.std(results)} ms")
+            results_torchvision = time_transform(
+                n_runs_single, input, torchvision_transform
+            )
+            mean_torchvision = float(results_torchvision.mean())
+            std_torchvision = float(torch.std(results_torchvision))
 
-for t in batch_torchvision_transforms:
-    results = torch.zeros(runs)
-    for i in range(runs):
-        torch.cuda.synchronize()
-        start = time.process_time()
-        torch.stack([t(img) for img in batch_input])
-        torch.cuda.synchronize()
-        end = time.process_time()
-        results[i] = end - start
-    results *= 1000
-    print(f"each CPU {type(t)} in {results.mean()} +/- {torch.std(results)} ms")
+            format_torchvision = f"{mean_torchvision:.2f} +/- {std_torchvision:.2f}"
 
-for t in batch_torchvision_transforms:
-    results = torch.zeros(runs)
-    for i in range(runs):
-        torch.cuda.synchronize()
-        start = time.process_time()
-        torch.stack([t(img) for img in batch_input_cuda])
-        torch.cuda.synchronize()
-        end = time.process_time()
-        results[i] = end - start
-    results *= 1000
-    print(f"each CUDA {type(t)} in {results.mean()} +/- {torch.std(results)} ms")
+            results_torchaug = time_transform(n_runs_single, input, torchaug_transform)
+            mean_torchaug = float(results_torchaug.mean())
+            std_torchaug = float(torch.std(results_torchaug))
 
+            format_torchaug = f"{mean_torchaug:.2f} +/- {std_torchaug:.2f}"
 
-for t in cpu_batch_transforms:
-    results = torch.zeros(runs)
-    for i in range(runs):
-        torch.cuda.synchronize()
-        start = time.process_time()
-        t(batch_input)
-        torch.cuda.synchronize()
-        end = time.process_time()
-        results[i] = end - start
-    results *= 1000
-    print(f"CPU {type(t)} in {results.mean()} +/- {torch.std(results)} ms")
+            if mean_torchvision < mean_torchaug:
+                format_torchvision = "**" + format_torchvision + "**"
+            elif mean_torchvision > mean_torchaug:
+                format_torchaug = "**" + format_torchaug + "**"
 
+            rows.append(
+                [
+                    name,
+                    format_torchvision,
+                    format_torchaug,
+                ]
+            )
 
-for t in cuda_batch_transforms:
-    results = torch.zeros(runs)
-    for i in range(runs):
-        torch.cuda.synchronize()
-        start = time.process_time()
-        t(batch_input_cuda)
-        torch.cuda.synchronize()
-        end = time.process_time()
-        results[i] = end - start
-    results *= 1000
-    print(f"CUDA {type(t)} in {results.mean()} +/- {torch.std(results)} ms")
+        header = [
+            f"Transform (input {device}: {input.shape})",
+            "Torchvision (ms)",
+            "Torchaug (ms)",
+        ]
+        print("\n\n")
+        print(tabulate.tabulate(rows, header, tablefmt="github"))
+        print("\n\n")
+
+        header = ["Transform", "Torchvision", "Torchaug"]
+
+    if run_batch:
+        batch_transforms = [
+            (
+                "BatchRandomColorJitter",
+                "1",
+                None,
+                BF.BatchRandomColorJitter(0.5, 0.5, 0.5, 0.1, 0.5, 1, inplace=True).to(
+                    device=device
+                ),
+            ),
+            (
+                "BatchRandomColorJitter",
+                "8",
+                None,
+                BF.BatchRandomColorJitter(0.5, 0.5, 0.5, 0.1, 0.5, 8, inplace=True).to(
+                    device=device
+                ),
+            ),
+            (
+                "BatchRandomColorJitter",
+                "-1",
+                T.RandomApply([T.ColorJitter(0.5, 0.5, 0.5, 0.1)], 0.5),
+                BF.BatchRandomColorJitter(0.5, 0.5, 0.5, 0.1, 0.5, -1, inplace=True).to(
+                    device=device
+                ),
+            ),
+            (
+                "BatchRandomGaussianBlur",
+                "",
+                T.RandomApply([T.GaussianBlur([23, 23], [0.1, 2.0])], 0.5),
+                BF.BatchRandomGaussianBlur([23, 23], [0.1, 2], 0.5, inplace=True).to(
+                    device=device
+                ),
+            ),
+            (
+                "BatchRandomGrayScale",
+                "",
+                T.RandomApply([T.Grayscale(num_output_channels=3)], 0.5),
+                BF.BatchRandomGrayScale(0.5, inplace=True).to(device=device),
+            ),
+            (
+                "BatchRandomHorizontalFlip",
+                "",
+                T.RandomHorizontalFlip(0.5),
+                BF.BatchRandomHorizontalFlip(0.5, inplace=True).to(device=device),
+            ),
+            (
+                "BatchRandomResizedCrop",
+                "1",
+                None,
+                BF.BatchRandomResizedCrop([224, 224], num_rand_calls=1).to(
+                    device=device
+                ),
+            ),
+            (
+                "BatchRandomResizedCrop",
+                "8",
+                None,
+                BF.BatchRandomResizedCrop([224, 224], num_rand_calls=8).to(
+                    device=device
+                ),
+            ),
+            (
+                "BatchRandomResizedCrop",
+                "16",
+                None,
+                BF.BatchRandomResizedCrop([224, 224], num_rand_calls=16).to(
+                    device=device
+                ),
+            ),
+            (
+                "BatchRandomResizedCrop",
+                "-1",
+                T.RandomResizedCrop([224, 224], antialias=True),
+                BF.BatchRandomResizedCrop([224, 224], num_rand_calls=-1).to(
+                    device=device
+                ),
+            ),
+            (
+                "BatchRandomSolarize",
+                "",
+                T.RandomSolarize(0.5, 0.5),
+                BF.BatchRandomSolarize(0.5, 0.5, inplace=True).to(device=device),
+            ),
+        ]
+
+        input = torch.randn(batch_size, *shape, device=device)
+
+        print("Testing batch transforms")
+        rows = []
+        for (
+            name,
+            rand_calls,
+            torchvision_transform,
+            torchaug_transform,
+        ) in batch_transforms:
+            print(name, rand_calls)
+
+            do_tv = torchvision_transform is not None
+            if do_tv:
+                results_torchvision = time_batch_transform_tv(
+                    n_runs_batch, input, torchvision_transform
+                )
+                mean_torchvision = float(results_torchvision.mean())
+                std_torchvision = float(torch.std(results_torchvision))
+
+            format_torchvision = (
+                f"{mean_torchvision:.2f} +/- {std_torchvision:.2f}" if do_tv else ""
+            )
+
+            results_torchaug = time_transform(n_runs_batch, input, torchaug_transform)
+            mean_torchaug = float(results_torchaug.mean())
+            std_torchaug = float(torch.std(results_torchaug))
+
+            format_torchaug = f"{mean_torchaug:.2f} +/- {std_torchaug:.2f}"
+
+            if do_tv and mean_torchvision < mean_torchaug:
+                format_torchvision = "**" + format_torchvision + "**"
+            elif do_tv and mean_torchvision > mean_torchaug:
+                format_torchaug = "**" + format_torchaug + "**"
+
+            rows.append(
+                [
+                    name,
+                    rand_calls,
+                    format_torchvision,
+                    format_torchaug,
+                ]
+            )
+        header = [
+            f"Transform (input {device}: {input.shape})",
+            "num_rand_calls",
+            "Torchvision (ms)",
+            "Torchaug (ms)",
+        ]
+
+        print("\n\n")
+        print(tabulate.tabulate(rows, header, tablefmt="github"))
+        print("\n\n")
